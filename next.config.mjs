b7e54545b8bfe859462ca3317319fd6f0d2109b4
jtwaugh <james.t.waugh@gmail.com>
2024-07-26 16:34:12 -0400

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    basePath: '/do-not-cut-the-rope',
+    assetPrefix: '/do-not-cut-the-rope/',
+    output: 'export'
+};
 
 export default nextConfig;
